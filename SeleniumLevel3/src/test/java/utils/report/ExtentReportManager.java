a8084cb5cfd659c3161d1f8810838317b819d4bf
Thanh Hoang <hoangmaithanh7772@gmail.com>
2020-03-19 13:43:02 +0700

    UpdateCode

--- SeleniumLevel3/src/test/java/utils/report/ExtentReportManager.java
+++ SeleniumLevel3/src/test/java/utils/report/ExtentReportManager.java
@@ -7,9 +7,11 @@ import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.ChartLocation;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
+import utils.common.Common;
+
 public class ExtentReportManager {
 	private static ExtentReports extent;
-    private static String reportFileName = "Test-Automation-Report.html";
+    private static String reportFileName = "Test-Automation-Report " + Common.getNowTime("MM.dd.yyyy - HH.mm.ss") + ".html";
     private static String fileSeperator = System.getProperty("file.separator");
     private static String reportFilepath = System.getProperty("user.dir") + fileSeperator + "TestReport";
     private static String reportFileLocation =  reportFilepath + fileSeperator + reportFileName;
--- SeleniumLevel3/src/test/java/utils/report/ExtentTestManager.java
+++ SeleniumLevel3/src/test/java/utils/report/ExtentTestManager.java
@@ -2,27 +2,40 @@ package utils.report;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 
 public class ExtentTestManager {
-	static Map<Integer, ExtentTest> extentTestMap = new HashMap<Integer, ExtentTest>();
+	static Map<String, ExtentTest> extentTestMap = new HashMap<String, ExtentTest>();
 	static ExtentReports extent = ExtentReportManager.getInstance();
 
 	public static synchronized ExtentTest getTest() {
-		return (ExtentTest) extentTestMap.get((int) (long) (Thread.currentThread().getId()));
+		return (ExtentTest) extentTestMap.get(String.valueOf(Thread.currentThread().getId()));
 	}
 
 	public static synchronized void endTest() {
 		extent.flush();
 	}
-
-	public static synchronized ExtentTest startTest(String testName) {
-		ExtentTest test = extent.createTest(testName);
-		extentTestMap.put((int) (long) (Thread.currentThread().getId()), test);
-		return test;
+	
+	public static synchronized ExtentTest startTest(String testName, ExtentTest parent) {
+		try {
+			ExtentTest test;
+			if (parent != null) {
+				test = parent.createNode(testName);
+				extentTestMap.put(String.valueOf(Thread.currentThread().getId()), test);
+			}
+			else {
+				test = extent.createTest(testName);
+				extentTestMap.put(testName, test);
+			}
+			return test;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
-	
+	public static synchronized boolean isTestExisted(String key) {
+		return extentTestMap.containsKey(key);
+	}
 }

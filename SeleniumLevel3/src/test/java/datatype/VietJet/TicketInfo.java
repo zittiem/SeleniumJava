017c99c6dda32e936bef4475ec9724c62eed8e60
vuquocdzung <dzungvq@users.noreply.github.com>
2020-03-27 02:10:17 +0700

    Update 27/03

---
+++ SeleniumLevel3/src/test/java/datatype/VietJet/TicketInfo.java
@@ -0,0 +1,231 @@
+package datatype.VietJet;
+
+public class TicketInfo {
+
+	private String departureFrom = null;
+	private String departureTo = null;
+	private String departureDate = null;
+	private String departureTime = null;
+	private double departureFare = 0;
+	private double departureCharge = 0;
+	private double departureTax = 0;
+	private double departureTotal = 0;
+	private String returnFrom = null;
+	private String returnTo = null;
+	private String returnDate = null;
+	private String returnTime = null;
+	private double returnFare = 0;
+	private double returnCharge = 0;
+	private double returnTax = 0;
+	private double returnTotal = 0;
+	private double grandTotal = 0;
+	private int numberOfAdults = 0;
+	private int numberOfChildren = 0;
+	private int numberOfInfants = 0;
+
+	public TicketInfo setBookingInfo(BookingInfo bI) {
+		this.departureFrom = bI.getOriginValue();
+		this.departureTo = bI.getDestinationValue();
+		this.departureDate = bI.getDepartDate();
+		this.returnFrom = bI.getDestinationValue();
+		this.returnTo = bI.getOriginValue();
+		this.returnDate = bI.getReturnDate();
+		this.numberOfAdults = bI.getNumberOfAdults();
+		this.numberOfChildren = bI.getNumberOfChilden();
+		this.numberOfInfants = bI.getNumberOfInfants();
+		return this;
+	}
+
+	public String getDepartureFrom() {
+		return departureFrom;
+	}
+
+	public void setDepartureFrom(String departureFrom) {
+		this.departureFrom = departureFrom;
+	}
+
+	public String getDepartureTo() {
+		return departureTo;
+	}
+
+	public void setDepartureTo(String departureTo) {
+		this.departureTo = departureTo;
+	}
+
+	public String getDepartureDate() {
+		return departureDate;
+	}
+
+	public void setDepartureDate(String departureDate) {
+		this.departureDate = departureDate;
+	}
+
+	public String getDepartureTime() {
+		return departureTime;
+	}
+
+	public void setDepartureTime(String departureTime) {
+		this.departureTime = departureTime;
+	}
+
+	public double getDepartureFare() {
+		return departureFare;
+	}
+
+	public void setDepartureFare(double departureFare) {
+		this.departureFare = departureFare;
+	}
+
+	public double getDepartureCharge() {
+		return departureCharge;
+	}
+
+	public void setDepartureCharge(double departureCharge) {
+		this.departureCharge = departureCharge;
+	}
+
+	public double getDepartureTax() {
+		return departureTax;
+	}
+
+	public void setDepartureTax(double departureTax) {
+		this.departureTax = departureTax;
+	}
+
+	public double getDepartureTotal() {
+		return departureTotal;
+	}
+
+	public void setDepartureTotal(double departureTotal) {
+		this.departureTotal = departureTotal;
+	}
+
+	public String getReturnFrom() {
+		return returnFrom;
+	}
+
+	public void setReturnFrom(String returnFrom) {
+		this.returnFrom = returnFrom;
+	}
+
+	public String getReturnTo() {
+		return returnTo;
+	}
+
+	public void setReturnTo(String returnTo) {
+		this.returnTo = returnTo;
+	}
+
+	public String getReturnDate() {
+		return returnDate;
+	}
+
+	public void setReturnDate(String returnDate) {
+		this.returnDate = returnDate;
+	}
+
+	public String getReturnTime() {
+		return returnTime;
+	}
+
+	public void setReturnTime(String returnTime) {
+		this.returnTime = returnTime;
+	}
+
+	public double getReturnFare() {
+		return returnFare;
+	}
+
+	public void setReturnFare(double returnFare) {
+		this.returnFare = returnFare;
+	}
+
+	public double getReturnCharge() {
+		return returnCharge;
+	}
+
+	public void setReturnCharge(double returnCharge) {
+		this.returnCharge = returnCharge;
+	}
+
+	public double getReturnTax() {
+		return returnTax;
+	}
+
+	public void setReturnTax(double returnTax) {
+		this.returnTax = returnTax;
+	}
+
+	public double getReturnTotal() {
+		return returnTotal;
+	}
+
+	public void setReturnTotal(double returnTotal) {
+		this.returnTotal = returnTotal;
+	}
+
+	public double getGrandTotal() {
+		return grandTotal;
+	}
+
+	public void setGrandTotal(double grandTotal) {
+		this.grandTotal = grandTotal;
+	}
+
+	public int getNumberOfAdults() {
+		return numberOfAdults;
+	}
+
+	public void setNumberOfAdults(int numberOfAdults) {
+		this.numberOfAdults = numberOfAdults;
+	}
+
+	public int getNumberOfChildren() {
+		return numberOfChildren;
+	}
+
+	public void setNumberOfChildren(int numberOfChildren) {
+		this.numberOfChildren = numberOfChildren;
+	}
+
+	public int getNumberOfInfants() {
+		return numberOfInfants;
+	}
+
+	public void setNumberOfInfants(int numberOfInfants) {
+		this.numberOfInfants = numberOfInfants;
+	}
+
+	
+	
+	
+	
+	public void showInfo() {
+		System.out.println("===================Ticket Information===================" + "\n- DepartureFrom: "
+				+ this.departureFrom + "\n- DepartureTo: " + this.departureTo + "\n- DepartureDate: "
+				+ this.departureDate + "\n- DepartureTime: " + this.departureTime + "\n- DepartureFare: "
+				+ this.departureFare + "\n- DepartureCharge: " + this.departureCharge + "\n- DepartureTax: "
+				+ this.departureTax + "\n- DepartureTotal: " + this.departureTotal + "\n- ReturnFrom: "
+				+ this.returnFrom + "\n- ReturnTo: " + this.returnTo + "\n- ReturnDate: " + this.returnDate
+				+ "\n- ReturnTime: " + this.returnTime + "\n- ReturnFare: " + this.returnFare + "\n- ReturnCharge: "
+				+ this.returnCharge + "\n- ReturnTax: " + this.returnTax + "\n- ReturnTotal: " + this.returnTotal
+				+ "\n- GrandTotal: " + this.grandTotal + "\n- NumberOfAdults: " + this.numberOfAdults
+				+ "\n- NumberOfChildren: " + this.numberOfChildren + "\n- NumberOfInfants: " + this.numberOfInfants
+				+ "\n=========================================================");
+	}
+
+	public enum FlightClass {
+		ECO("Eco"), PROMO("Promo"), SKYBOSS("Skyboss");
+
+		private String flightClass;
+
+		FlightClass(String flightClass) {
+			this.flightClass = flightClass;
+		}
+
+		public String getValue() {
+			return this.flightClass;
+		}
+	}
+
+}
---
+++ SeleniumLevel3/src/test/java/tests/VietJet/TestCases.java
@@ -0,0 +1,60 @@
+package tests.VietJet;
+
+import org.testng.annotations.Test;
+
+import datatype.VietJet.BookingInfo;
+import datatype.VietJet.LanguageType;
+import datatype.VietJet.TicketInfo;
+import datatype.VietJet.TicketInfo.FlightClass;
+import pages.VietJet.HomePage;
+import pages.VietJet.PassengerInformationPage;
+import pages.VietJet.SelectTravelOptionsPage;
+import tests.TestBase;
+import utils.assertion.SoftAssertion;
+import utils.common.Constants;
+import utils.helper.DataHelper;
+import utils.helper.Logger;
+
+public class TestCases extends TestBase {
+	@Test(description = "TC_VJ_001, Verify that user can search and choose tickets on a specific day successfully.")
+	public void TC01() throws Exception {
+		Logger.info("Precondition: Initial Data");
+		DataHelper dataHelper = new DataHelper(Constants.DATA_FOLDER + "VietJet", new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		SoftAssertion softAssert = new SoftAssertion();
+		HomePage homePage = new HomePage();
+		BookingInfo booking = dataHelper.mapDataToObject(BookingInfo.class).compileData();
+		Logger.info("1. Navigate to 'https://www.vietjetair.com/Sites/Web/en-US/Home'.");
+		launchApp("VietJet", LanguageType.US);
+		Logger.info("2. Search the ticket with the following information");
+		homePage.searchFlight(booking);
+		Logger.verify("Select Travel Options page is displayed");
+		SelectTravelOptionsPage traveloptionPage = new SelectTravelOptionsPage();
+		softAssert.assertTrue(traveloptionPage.isDisplayed(), "Select Travel Options Page does not display.");
+		Logger.verify("The ticket price is displayed in VND");
+		softAssert.assertTrue(traveloptionPage.isCurrency(booking.getCurrency()),
+				"The ticket price is not displayed in VND");
+		Logger.verify("The flights dates are displayed correctly");
+		Logger.verify("The departure and arrival places are correct.");
+		Logger.verify("Number of passenger is correct");
+		BookingInfo actBooking = traveloptionPage.getCurrentBookingInfo();
+		actBooking.showInfo();
+		softAssert.assertObjectEquals(actBooking, booking, "Booking info does not display properly.");
+		Logger.info("3. Choose the cheapest tickets and click 'Continue' button");
+		traveloptionPage.selectCheapestTicket(FlightClass.PROMO);
+		TicketInfo ticket = traveloptionPage.getTicketDetails();
+		traveloptionPage.submitPage();
+		PassengerInformationPage passengerInfoPage = new PassengerInformationPage();
+		Logger.verify("Passenger Information page is displayed");
+		softAssert.assertTrue(passengerInfoPage.isDisplayed(), "Passenger Information Page display.");
+		Logger.verify("Tickets information is correct");
+		TicketInfo actTicket = passengerInfoPage.getCurrentTicketInfo();
+		softAssert.assertObjectEquals(actTicket, ticket, "Tickets information is not correct");
+		softAssert.assertAll();
+	}
+
+	@Test(description = "TC_VJ_003, Verify that user can search and choose cheapest tickets on next 3 months successfully.")
+	public void TC03() {
+	}
+
+}
